3571cb200f571eac1a8491551389fb3efbc95b4d
Mohammad Najm <najm.devops@gmail.com>
2025-05-25 13:18:25 +0530

    Initial commit

---
+++ src/main/java/com/knightriders/transactiontool/TransactiontoolApplication.java
@@ -0,0 +1,13 @@
+package com.knightriders.transactiontool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactiontoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactiontoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knightriders/transactiontool/TransactiontoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knightriders.transactiontool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactiontoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
